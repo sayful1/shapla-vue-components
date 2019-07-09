4a7081ecdcf684342149fea40dc1cd01f95f94d2
Sayful Islam <sayful.islam001@gmail.com>
2019-07-10 00:41:00 +0530

    Add test components for testing component.

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+        <test-columns></test-columns>
+        <test-delete></test-delete>
+        <test-modal></test-modal>
+    </div>
+</template>
+
+<script>
+
+    import TestColumns from "./components/TestColumns";
+    import TestDelete from "./components/TestDelete";
+    import TestModal from "./components/TestModal";
+
+    export default {
+        name: 'app',
+        components: {
+            TestModal,
+            TestDelete,
+            TestColumns
+        }
+    }
+</script>
+
+<style>
+    html {
+        background-color: #fff;
+        font-size: 16px;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-size-adjust: 100%;
+        min-width: 320px;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        text-rendering: optimizeLegibility;
+    }
+
+    body, button, input, select, textarea {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+        font-size: 1rem;
+        line-height: 1.5;
+        color: #323232;
+        text-transform: none;
+        text-align: left;
+    }
+
+    #app {
+        padding: 1rem;
+    }
+</style>
---
+++ src/components/TestModal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>Modals</h2>
+        <columns>
+            <column>
+                <button @click="openCardModal = true">Open Card Modal</button>
+            </column>
+        </columns>
+
+        <modal :active="openCardModal" @close="openCardModal = false" title="Modal Demo 1">
+           Medium width modal.
+        </modal>
+    </div>
+</template>
+
+<script>
+    import {columns, column} from '../../shapla-columns'
+    import modal from '../../shapla-modal';
+
+    export default {
+        name: "TestModal",
+        components: {
+            modal,
+            columns,
+            column
+        },
+        data() {
+            return {
+                openCardModal: false,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
