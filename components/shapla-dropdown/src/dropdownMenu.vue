ee43525af609f53adfb15b5f8b3f1ce1238e2372
Sayful Islam <sayful.islam001@gmail.com>
2020-03-12 13:06:36 +0530

    Add auto direction (up/down) for shapla-dropdown component.

--- components/shapla-dropdown/src/dropdownMenu.vue
+++ components/shapla-dropdown/src/dropdownMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="shapla-dropdown-menu" :class="menuClasses" :role="role">
+    <div class="shapla-dropdown-menu" :class="classes" :role="role">
         <div class="shapla-dropdown-menu__content">
             <slot></slot>
         </div>
@@ -10,28 +10,70 @@
     export default {
         name: "dropdownMenu",
         props: {
-            active: {type: Boolean, default: false},
-            role: {type: String, default: 'menu'},
-            right: {type: Boolean, default: false},
-            up: {type: Boolean, default: false},
+            active: {
+                type: Boolean,
+                default: false
+            },
+            role: {
+                type: String,
+                default: 'menu'
+            },
+            right: {
+                type: Boolean,
+                default: false
+            },
+            up: {
+                type: Boolean,
+                default: false
+            },
+            direction: {
+                type: String,
+                default: 'auto',
+                validator: value => ['auto', 'up', 'down'].indexOf(value) !== -1
+            },
         },
-        computed: {
-            menuClasses() {
-                let classes = [];
-
-                if (this.right) {
-                    classes.push('is-right');
+        data() {
+            return {
+                classes: [],
+            }
+        },
+        watch: {
+            active(isActive) {
+                if (isActive) {
+                    if (this.direction === 'auto') {
+                        this.calculateDirection();
+                    }
+                    this.classes.push('is-active');
+                } else {
+                    this.classes.splice(this.classes.indexOf('is-active'), 1);
                 }
+            }
+        },
+        mounted() {
+            if (this.right) {
+                this.classes.push('is-right');
+            }
 
-                if (this.up) {
-                    classes.push('is-up');
+            if (this.up || this.direction === 'up') {
+                this.classes.push('is-up');
+            }
+        },
+        methods: {
+            calculateDirection() {
+                let browserHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+                    elParent = this.$el.parentNode,
+                    rect = elParent.getBoundingClientRect(),
+                    win = elParent.ownerDocument.defaultView,
+                    elFromTop = parseInt(rect.top + win.pageYOffset),
+                    spaceToBottom = browserHeight - elFromTop;
+
+                let index = this.classes.indexOf('is-up');
+                if (spaceToBottom > elFromTop && -1 !== index) {
+                    this.classes.splice(index, 1);
                 }
-
-                if (this.active) {
-                    classes.push('is-active');
+                if (spaceToBottom < elFromTop && -1 === index) {
+                    this.classes.push('is-up');
                 }
-
-                return classes;
             }
         }
     }
