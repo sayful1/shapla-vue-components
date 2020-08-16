f671a8f9b47f799606f1695096c8437880193b6c
Sayful Islam <sayful.islam001@gmail.com>
2020-08-16 23:20:50 +0530

    Update chip and columns example file.

--- examples/src/components/TestChip.vue
+++ examples/src/components/TestChip.vue
@@ -1,49 +1,89 @@
 <template>
   <div>
-    <div>
-      <h3>Default Chip</h3>
-      <shapla-chip>Sayful Islam</shapla-chip>
-    </div>
-    <div>
-      <h3>Default Chip (small)</h3>
-      <shapla-chip small>Sayful Islam</shapla-chip>
+    <h3 class="section-title">Default Chip</h3>
+    <div class="section">
+      <columns>
+        <column :tablet="5">
+          <shapla-chip>Default Chip</shapla-chip>
+          <br/>
+          <shapla-chip small>Default Chip (small)</shapla-chip>
+          <br/>
+          <shapla-chip :deletable="true">Deletable Chip</shapla-chip>
+          <br/>
+          <shapla-chip :deletable="true" :small="true">Deletable Chip (small)</shapla-chip>
+        </column>
+        <column :tablet="7">
+          <code-highlight>
+            <pre>
+              <code class="html">&lt;shapla-chip&gt;Default Chip&lt;/shapla-chip&gt;
+&lt;shapla-chip small&gt;Default Chip (small)&lt;/shapla-chip&gt;
+&lt;shapla-chip :deletable=&quot;true&quot;&gt;Deletable Chip&lt;/shapla-chip&gt;
+&lt;shapla-chip :deletable=&quot;true&quot; :small=&quot;true&quot;&gt;Deletable Chip (small)&lt;/shapla-chip&gt;</code>
+            </pre>
+          </code-highlight>
+        </column>
+      </columns>
     </div>
 
-    <div>
-      <h3>Deletable Chip</h3>
-      <shapla-chip text="Sayful Islam" :deletable="true"/>
-    </div>
-
-    <div>
-      <h3>Deletable Chip (small)</h3>
-      <shapla-chip text="Sayful Islam" :deletable="true" :small="true"/>
-    </div>
-
-    <div>
-      <h3>Deletable Chip with Contact</h3>
-      <shapla-chip text="Sayful Islam" :deletable="true"
-                   image_src="https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&default=retro"
-      />
-    </div>
-
-    <div>
-      <h3>Deletable Chip with Contact (small)</h3>
-      <shapla-chip
-        text="Sayful Islam"
-        image_src="https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&default=retro"
-        :deletable="true"
-        :small="true"
-      />
+    <h3 class="section-title">Chip with Contact</h3>
+    <div class="section">
+      <columns>
+        <column :tablet="5">
+          <shapla-chip
+            image_src="https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&default=retro"
+            text="Chip with Contact"
+          />
+          <br/>
+          <shapla-chip
+            image_src="https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&default=retro"
+            text="Deletable Chip with Contact" :deletable="true"
+          />
+          <br/>
+          <shapla-chip
+            image_src="https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&default=retro"
+            text="Chip with Contact (small)" :small="true"
+          />
+          <br/>
+          <shapla-chip
+            image_src="https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&default=retro"
+            text="Deletable Chip with Contact (small)" :deletable="true" :small="true"
+          />
+        </column>
+        <column :tablet="7">
+          <code-highlight>
+            <pre>
+              <code class="html">&lt;shapla-chip
+ image_src=&quot;https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&amp;default=retro&quot;
+ text=&quot;Chip with Contact&quot;
+/&gt;
+&lt;shapla-chip
+ image_src=&quot;https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&amp;default=retro&quot;
+ text=&quot;Deletable Chip with Contact&quot; :deletable=&quot;true&quot;
+/&gt;
+&lt;shapla-chip
+ image_src=&quot;https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&amp;default=retro&quot;
+ text=&quot;Chip with Contact (small)&quot; :small=&quot;true&quot;
+/&gt;
+&lt;shapla-chip
+ image_src=&quot;https://s.gravatar.com/avatar/5ba82fcf5f7f8b24097ff9f7ad4b3d5b?size=100&amp;default=retro&quot;
+ text=&quot;Deletable Chip with Contact (small)&quot; :deletable=&quot;true&quot; :small=&quot;true&quot;
+/&gt;</code>
+            </pre>
+          </code-highlight>
+        </column>
+      </columns>
     </div>
   </div>
 </template>
 
 <script>
 import shaplaChip from "shapla-chip";
+import {columns, column} from 'shapla-columns'
+import CodeHighlight from "@/CodeHighlight";
 
 export default {
   name: "TestChip",
-  components: {shaplaChip},
+  components: {CodeHighlight, shaplaChip, columns, column},
 }
 </script>
 
--- examples/src/components/TestColumns.vue
+++ examples/src/components/TestColumns.vue
@@ -1,93 +1,123 @@
 <template>
   <div class="shapla-columns-example">
-    <h2>Columns with default gap (0.75rem)</h2>
-    <columns>
-      <column>
-        <div class="card-column-box">Column example 1</div>
-      </column>
-      <column>
-        <div class="card-column-box">Column example 2</div>
-      </column>
-      <column>
-        <div class="card-column-box">Column example 3</div>
-      </column>
-      <column>
-        <div class="card-column-box">Column example 4</div>
-      </column>
-    </columns>
+    <h2 class="section-title">Columns with default gap (0.75rem)</h2>
+    <section class="section">
+      <columns>
+        <column :tablet="5">
+          <columns>
+            <column>
+              <div class="card-column-box">25% width</div>
+            </column>
+            <column>
+              <div class="card-column-box">25% width</div>
+            </column>
+            <column :tablet="6">
+              <div class="card-column-box">50% width</div>
+            </column>
+          </columns>
+        </column>
+        <column :tablet="7">
+          <code-highlight>
+            <pre>
+              <code class="html">&lt;columns&gt;
+ &lt;column&gt;
+    &lt;div class=&quot;card-column-box&quot;&gt;25% width&lt;/div&gt;
+ &lt;/column&gt;
+ &lt;column&gt;
+    &lt;div class=&quot;card-column-box&quot;&gt;25% width&lt;/div&gt;
+ &lt;/column&gt;
+ &lt;column :tablet=&quot;6&quot;&gt;
+    &lt;div class=&quot;card-column-box&quot;&gt;50% width&lt;/div&gt;
+ &lt;/column&gt;
+&lt;/columns&gt;</code>
+            </pre>
+          </code-highlight>
+        </column>
+      </columns>
+    </section>
 
-    <h2>Columns with custom gap (1.25rem)</h2>
-    <columns multiline variable column-gap="1.24rem">
-      <column :tablet="4">
-        <div class="card-column-box">Column example 1</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 2</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 3</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 4</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 1</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 2</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 3</div>
-      </column>
-      <column :tablet="4">
-        <div class="card-column-box">Column example 4</div>
-      </column>
-    </columns>
-
-    <h2>Columns with default gap (0.75rem)</h2>
-    <columns>
-      <column>
-        <div class="card-column-box">Column example 1</div>
-      </column>
-      <column>
-        <div class="card-column-box">Column example 2</div>
-      </column>
-      <column>
-        <div class="card-column-box">Column example 3</div>
-      </column>
-      <column>
-        <div class="card-column-box">Column example 4</div>
-      </column>
-    </columns>
-
+    <h2 class="section-title">Columns with custom gap (1.25rem)</h2>
+    <section class="section">
+      <columns>
+        <column :tablet="5">
+          <columns multiline variable column-gap="1.24rem">
+            <column :tablet="4">
+              <div class="card-column-box">Column example 1</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 2</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 3</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 4</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 1</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 2</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 3</div>
+            </column>
+            <column :tablet="4">
+              <div class="card-column-box">Column example 4</div>
+            </column>
+          </columns>
+        </column>
+        <column :tablet="7">
+          <code-highlight>
+            <pre>
+              <code class="html">&lt;columns multiline variable column-gap=&quot;1.24rem&quot;&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 1&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 2&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 3&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 4&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 1&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 2&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 3&lt;/div&gt;
+	&lt;/column&gt;
+	&lt;column :tablet=&quot;4&quot;&gt;
+		&lt;div class=&quot;card-column-box&quot;&gt;Column example 4&lt;/div&gt;
+	&lt;/column&gt;
+&lt;/columns&gt;</code>
+            </pre>
+          </code-highlight>
+        </column>
+      </columns>
+    </section>
   </div>
 </template>
 
 <script>
 import {columns, column} from 'shapla-columns';
+import CodeHighlight from "@/CodeHighlight";
 
 export default {
   name: "TestColumns",
-  components: {columns, column}
+  components: {CodeHighlight, columns, column}
 }
 </script>
 
 <style lang="scss">
-.shapla-columns-example {
-  h2 {
-    font-size: 20px;
-    margin-bottom: 10px;
-    border-bottom: 1px solid rgba(#000, 0.12);
-    padding-bottom: 10px;
-
-    &:not(:first-child) {
-      margin-top: 20px;
-    }
-  }
-}
-
 .card-column-box {
   background: rgba(#000, 0.12);
   padding: 5px;
+  text-align: center;
 }
 </style>
