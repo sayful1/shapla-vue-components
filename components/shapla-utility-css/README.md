a059a94a6fe64d9abcb16f4b2474fa7855a8d752
Sayful Islam <sayful.islam001@gmail.com>
2020-09-06 20:10:04 +0530

    Add responsive margin and padding.

--- components/shapla-utility-css/README.md
+++ components/shapla-utility-css/README.md
@@ -13,5 +13,33 @@ Apply helper classes to almost any element, in order to alter their style.
 npm i shapla-utility-css
 ```
 
+# Available Utility Classes
+* Accessibility
+    * Screen Readers
+* Effects
+    * Box Shadow
+* Flexbox
+    * Align Content
+    * Align Items
+    * Align Self
+    * Display
+    * Flex
+    * Flex Direction
+    * Flex Grow
+    * Flex Shrink
+    * Flex Wrap
+    * Justify Content
+    * Order
+* Layout
+    * Container
+    * Visibility
+    * Responsive Visibility
+* Spacing
+    * Margin
+    * Responsive Margin
+    * Padding
+    * Responsive Padding
+* SVG
+
 # Usage
 Coming soon...
---
+++ components/shapla-utility-css/webpack.config.js
@@ -0,0 +1,148 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const combineMediaQuery = require('postcss-combine-media-query');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const svgToMiniDataURI = require('mini-svg-data-uri');
+
+let plugins = [];
+let entryPoints = {
+  utility: [
+    "./src/utility.scss",
+  ],
+};
+
+plugins.push(new MiniCssExtractPlugin({
+  filename: "./[name].css"
+}));
+
+plugins.push(new VueLoaderPlugin());
+
+module.exports = (env, argv) => {
+  let isDev = argv.mode !== 'production';
+
+  return {
+    "entry": entryPoints,
+    "output": {
+      "path": path.resolve(__dirname, 'dist'),
+      "filename": '[name].js'
+    },
+    "devtool": isDev ? 'eval-source-map' : false,
+    "module": {
+      "rules": [
+        {
+          "test": /\.js$/i,
+          "use": {
+            "loader": "babel-loader",
+            "options": {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.vue$/i,
+          use: [
+            {loader: 'vue-loader'}
+          ]
+        },
+        {
+          test: /\.(sass|scss|css)$/i,
+          use: [
+            {
+              loader: isDev ? "style-loader" : MiniCssExtractPlugin.loader
+            },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: isDev,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: isDev,
+                plugins: () => [
+                  autoprefixer(),
+                  combineMediaQuery()
+                ],
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: isDev,
+              },
+            }
+          ]
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: '../fonts',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|je?pg|gif)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192, // 8KB
+                outputPath: '../images',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10240, // 10KB
+                outputPath: '../images',
+                generator: (content) => svgToMiniDataURI(content.toString()),
+              },
+            },
+          ],
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new OptimizeCSSAssetsPlugin()
+      ],
+      splitChunks: {
+        cacheGroups: {
+          commonVendors: {
+            test: /[\\/]node_modules[\\/](vue|vuex|vue-router|axios)[\\/]/,
+            name: 'common-vendors',
+            chunks: 'all',
+          }
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@': path.resolve('./assets/src/'),
+      },
+      modules: [
+        path.resolve('./node_modules'),
+        path.resolve(path.join(__dirname, 'assets/src/')),
+        path.resolve(path.join(__dirname, 'assets/src/shapla')),
+      ],
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    "plugins": plugins
+  }
+};
