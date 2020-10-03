990adb7f96d5af63984e8c43420a39b724a79872
Sayful Islam <sayful.islam001@gmail.com>
2020-10-03 18:24:58 +0530

    Update simple-spinner style.

--- components/shapla-spinner/example/SpinnerExample.vue
+++ components/shapla-spinner/example/SpinnerExample.vue
@@ -2,38 +2,26 @@
   <div id="app" class="shapla-vue-components--demo">
     <div class="container mx-auto">
       <div class="flex space-x-4 p-4">
-        <spinner position="static"/>
-
-        <spinner position="static" single/>
-
-        <simple-spinner size="32px"/>
-        <simple-spinner size="48px" stroke-width="4"/>
-        <simple-spinner size="48px" single/>
+        <!--        <simple-spinner size="32px"/>-->
+        <!--        <simple-spinner size="48px" stroke-width="4"/>-->
+        <simple-spinner size="48px" single show-text/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import spinner from "../src/spinner";
 import SimpleSpinner from "../src/SimpleSpinner";
 
 export default {
   name: 'SpinnerExample',
-  components: {spinner, SimpleSpinner},
+  components: {SimpleSpinner},
   data() {
-    return {
-      steps: [
-        {key: 'login', number_text: '1', tab_text: 'Login', completed: true},
-        {key: 'shipping', number_text: '2', tab_text: 'Shipping', completed: true},
-        {key: 'billing', number_text: '3', tab_text: 'Billing', current: true},
-        {key: 'order-payment', number_text: '4', tab_text: 'Order & Payment'},
-      ]
-    }
+    return {}
   }
 }
 </script>
 
 <style lang="scss">
-@import url("https://unpkg.com/shapla-utility-css@1.0.10/dist/utility.css");
+// @import "~shapla-utility-css/src/all";
 </style>
--- components/shapla-spinner/src/SimpleSpinner.vue
+++ components/shapla-spinner/src/SimpleSpinner.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-if="active" :class="containerClass">
-    <div class="shapla-spinner-inner" :class="{'has-text':showText}">
-      <svg class="shapla-simple-spinner" :width="size" :height="size" viewBox="0 0 66 66"
-           xmlns="http://www.w3.org/2000/svg">
-        <circle class="shapla-simple-spinner__circle" :class="{'is-single':single}" fill="none"
-                :stroke-width="strokeWidth" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
-      </svg>
-      <div class="shapla-spinner-text" v-if="showText">Loading...</div>
+  <div v-if="active" class="shapla-simple-spinner-container flex items-center justify-center" :class="containerClass">
+    <div class="shapla-simple-spinner-inner flex items-center justify-center flex-row" :class="innerClasses">
+      <slot>
+        <svg class="shapla-simple-spinner" :width="size" :height="size" viewBox="0 0 66 66"
+             xmlns="http://www.w3.org/2000/svg">
+          <circle class="shapla-simple-spinner__circle" :class="{'is-single':single}" fill="none"
+                  :stroke-width="strokeWidth" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+        </svg>
+      </slot>
+      <div class="shapla-simple-spinner-text m-2" v-if="showText">Loading...</div>
     </div>
   </div>
 </template>
@@ -18,6 +20,7 @@ export default {
     active: {type: Boolean, default: true},
     single: {type: Boolean, default: false},
     showText: {type: Boolean, default: false},
+    theme: {type: String, default: 'light', validator: value => ['light', 'dark'].indexOf(value) !== -1},
     position: {type: String, default: 'fixed'},
     size: {type: String, default: '28px'},
     strokeWidth: {type: String, default: '3px'},
@@ -25,84 +28,24 @@ export default {
   computed: {
     containerClass() {
       return {
-        'shapla-simple-spinner-container': true,
         'is-fixed': this.position === 'fixed',
         'is-absolute': this.position === 'absolute',
         'is-static': this.position === 'static',
+        'is-light-theme': this.theme === 'light',
+        'is-dark-theme': this.theme === 'dark',
       }
     },
+    innerClasses() {
+      let classes = [];
+      if (this.showText) {
+        classes.push('has-text p-4 shadow-3');
+      }
+      return classes;
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-$offset: 187;
-$duration: 1.4s;
-
-@keyframes shapla-spinner__container-rotate {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(270deg);
-  }
-}
-
-@keyframes shapla-spinner__circle-colors {
-  0% {
-    stroke: #42A5F5;
-  }
-  25% {
-    stroke: #f44336;
-  }
-  50% {
-    stroke: #FDD835;
-  }
-  75% {
-    stroke: #4CAF50;
-  }
-  100% {
-    stroke: #42A5F5;
-  }
-}
-
-@keyframes shapla-spinner__circle-dash {
-  0% {
-    stroke-dashoffset: $offset;
-  }
-  50% {
-    stroke-dashoffset: $offset / 4;
-    transform: rotate(135deg);
-  }
-  100% {
-    stroke-dashoffset: $offset;
-    transform: rotate(450deg);
-  }
-}
-
-.shapla-simple-spinner-container {
-  --spinner-duration: #{$duration};
-  --spinner-offset: #{$offset};
-
-  &.is-static {
-
-  }
-}
-
-.shapla-simple-spinner {
-  animation: shapla-spinner__container-rotate var(--spinner-duration) linear infinite;
-}
-
-.shapla-simple-spinner__circle {
-  stroke-dasharray: var(--spinner-offset);
-  stroke-dashoffset: 0;
-  transform-origin: center;
-  stroke: #42A5F5;
-  animation: shapla-spinner__circle-dash var(--spinner-duration) ease-in-out infinite,
-  shapla-spinner__circle-colors calc(var(--spinner-duration) * 4) ease-in-out infinite;
-
-  &.is-single {
-    animation: shapla-spinner__circle-dash var(--spinner-duration) ease-in-out infinite;
-  }
-}
+<style lang="scss">
+@import "simple-spinner";
 </style>
