8d7e64c15a4b639ee0a94c9e436f6db35983a37d
Sayful Islam <sayful.islam001@gmail.com>
2020-10-12 15:30:44 +0530

    Update shapla-checkbox component.

--- components/shapla-checkbox/src/index.js
+++ components/shapla-checkbox/src/index.js
@@ -1,5 +1,5 @@
 /*!
- * Shapla Checkbox v1.0.4
+ * Shapla Checkbox v1.0.5
  * (c) 2020 Sayful Islam
  * Released under the MIT License.
  */
@@ -7,9 +7,9 @@ import shaplaCheckbox from './shaplaCheckbox'
 
 // Declare install function executed by Vue.use()
 export function install(Vue) {
-    if (install.installed) return;
-    install.installed = true;
-    Vue.component('shapla-checkbox', shaplaCheckbox);
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('shapla-checkbox', shaplaCheckbox);
 }
 
 // Create module definition for Vue.use()
@@ -18,13 +18,14 @@ const plugin = {install};
 // Auto-install when vue is found (eg. in browser via <script> tag)
 let GlobalVue = null;
 if (typeof window !== 'undefined') {
-    GlobalVue = window.Vue;
+  GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
-    GlobalVue = global.Vue;
+  GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-    GlobalVue.use(plugin);
+  GlobalVue.use(plugin);
 }
 
 // To allow use as module (npm/webpack/etc.) export component
+export {shaplaCheckbox}
 export default shaplaCheckbox;
