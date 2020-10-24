c15f5faed1341371989fd17a5973b1d164292370
Sayful Islam <sayful.islam001@gmail.com>
2020-10-24 18:27:02 +0530

    Update spinner component.

--- components/shapla-spinner/example/SpinnerExample.vue
+++ components/shapla-spinner/example/SpinnerExample.vue
@@ -2,20 +2,18 @@
   <div id="app" class="shapla-vue-components--demo">
     <div class="container mx-auto">
       <div class="flex space-x-4 p-4">
-        <!--        <simple-spinner size="32px"/>-->
-        <!--        <simple-spinner size="48px" stroke-width="4"/>-->
-        <simple-spinner size="48px" single show-text/>
+        <spinner size="48px" :single="false" :show-text="false"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import SimpleSpinner from "../src/SimpleSpinner";
+import spinner from "../src/spinner";
 
 export default {
   name: 'SpinnerExample',
-  components: {SimpleSpinner},
+  components: {spinner},
   data() {
     return {}
   }
--- components/shapla-spinner/src/spinner.vue
+++ components/shapla-spinner/src/spinner.vue
@@ -1,88 +1,75 @@
 <template>
-    <div :class="containerClass" v-if="active">
-        <div class="shapla-spinner-inner" :class="{'has-text':showText}">
-            <div class="shapla-spinner" :class="getClass">
-                <div :class="itemClass(index)" :style="itemStyle(index)" v-for="index in [1, 2, 3, 4]" :key="index">
-                    <div class="shapla-spinner__circle-clipper shapla-spinner__left">
-                        <div class="shapla-spinner__circle"></div>
-                    </div>
-                    <div class="shapla-spinner__gap-patch">
-                        <div class="shapla-spinner__circle"></div>
-                    </div>
-                    <div class="shapla-spinner__circle-clipper shapla-spinner__right">
-                        <div class="shapla-spinner__circle"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="shapla-spinner-text" v-if="showText">Loading...</div>
+  <div :class="containerClass" v-if="active">
+    <div class="shapla-spinner-inner" :class="{'has-text':showText}">
+      <div class="shapla-spinner" :class="getClass">
+        <div :class="itemClass(index)" v-for="index in [1, 2, 3, 4]" :key="index">
+          <div class="shapla-spinner__circle-clipper shapla-spinner__left">
+            <div class="shapla-spinner__circle"></div>
+          </div>
+          <div class="shapla-spinner__gap-patch">
+            <div class="shapla-spinner__circle"></div>
+          </div>
+          <div class="shapla-spinner__circle-clipper shapla-spinner__right">
+            <div class="shapla-spinner__circle"></div>
+          </div>
         </div>
+      </div>
+      <div class="shapla-spinner-text" v-if="showText">Loading...</div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "spinner",
-        props: {
-            active: {type: Boolean, default: true},
-            single: {type: Boolean, default: false},
-            showText: {type: Boolean, default: false},
-            position: {type: String, default: 'fixed'},
-            primaryColor: {type: String, default: '#3F51B5'},
-            layer1Color: {type: String, default: '#42A5F5'},
-            layer2Color: {type: String, default: '#f44336'},
-            layer3Color: {type: String, default: '#FDD835'},
-            layer4Color: {type: String, default: '#4CAF50'},
-        },
-        computed: {
-            containerClass() {
-                return {
-                    'shapla-spinner-container': true,
-                    'is-fixed': this.position === 'fixed',
-                    'is-absolute': this.position === 'absolute',
-                    'is-static': this.position === 'static',
-                }
-            },
-            getClass() {
-                return {
-                    'shapla-spinner--single-color': this.single,
-                }
-            }
-        },
-        watch: {
-            active(newValue) {
-                this.refreshBodyClass(newValue);
-            }
-        },
-
-        mounted() {
-            this.refreshBodyClass(this.active);
-        },
-        methods: {
-            itemClass(index) {
-                return [
-                    'shapla-spinner__layer',
-                    'shapla-spinner__layer-' + index
-                ]
-            },
-            itemStyle(index) {
-                if (this.single) return {borderColor: this.primaryColor};
-                if (index === 1) return {borderColor: this.layer1Color};
-                if (index === 2) return {borderColor: this.layer2Color};
-                if (index === 3) return {borderColor: this.layer2Color};
-                if (index === 4) return {borderColor: this.layer4Color};
-                return {borderColor: this.primaryColor};
-            },
-            refreshBodyClass(active) {
-                if (active) {
-                    document.querySelector('body').classList.add('has-shapla-spinner');
-                } else {
-                    document.querySelector('body').classList.remove('has-shapla-spinner');
-                }
-            }
-        }
+export default {
+  name: "spinner",
+  props: {
+    active: {type: Boolean, default: true},
+    single: {type: Boolean, default: false},
+    showText: {type: Boolean, default: false},
+    position: {type: String, default: 'fixed'},
+  },
+  computed: {
+    containerClass() {
+      return {
+        'shapla-spinner-container': true,
+        'is-fixed': this.position === 'fixed',
+        'is-absolute': this.position === 'absolute',
+        'is-static': this.position === 'static',
+      }
+    },
+    getClass() {
+      return {
+        'shapla-spinner--single-color': this.single,
+      }
     }
+  },
+  watch: {
+    active(newValue) {
+      this.refreshBodyClass(newValue);
+    }
+  },
+
+  mounted() {
+    this.refreshBodyClass(this.active);
+  },
+  methods: {
+    itemClass(index) {
+      return [
+        'shapla-spinner__layer',
+        'shapla-spinner__layer-' + index
+      ]
+    },
+    refreshBodyClass(active) {
+      if (active) {
+        document.querySelector('body').classList.add('has-shapla-spinner');
+      } else {
+        document.querySelector('body').classList.remove('has-shapla-spinner');
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-    @import "spinner";
+@import "spinner";
 </style>
