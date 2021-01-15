971a6a9c835653fd88306871196cb69b51473444
Sayful Islam <sayful.islam001@gmail.com>
2021-01-15 13:04:51 +0530

    Fix header cell icon is not showing.

--- components/shapla-data-table/src/components/HeaderCell.vue
+++ components/shapla-data-table/src/components/HeaderCell.vue
@@ -1,13 +1,15 @@
 <template>
   <th :class="getHeadCellClass" role="columnheader" scope="col">
-    <template v-if="!isSortable">{{ column.label }}</template>
-    <template v-else>
-      <a href="#" @click.prevent="handleSort">
-        <data-table-icon v-if="isSortedDesc" icon="arrow-upward"/>
-        <data-table-icon v-if="isSortedAsc" icon="arrow-downward"/>
-        <span>{{ column.label }}</span>
-      </a>
-    </template>
+    <slot>
+      <template v-if="!isSortable">{{ column.label }}</template>
+      <template v-else>
+        <a href="#" @click.prevent="handleSort">
+          <data-table-icon v-if="isSortedDesc" icon="arrow-upward"/>
+          <data-table-icon v-if="isSortedAsc" icon="arrow-downward"/>
+          <span>{{ column.label }}</span>
+        </a>
+      </template>
+    </slot>
   </th>
 </template>
 
@@ -18,12 +20,15 @@ export default {
   name: "HeaderCell",
   components: {DataTableIcon},
   props: {
-    column: {type: Object, required: true},
+    column: {type: Object, required: false},
     isPrimary: {type: Boolean, default: false},
     sortBy: {type: String, default: 'id'},
     sortOrder: {type: String, default: "desc"},
   },
   computed: {
+    isNumeric() {
+      return (typeof this.column.numeric !== "undefined" && this.column.numeric === true);
+    },
     isSortable() {
       return (typeof this.column.sortable !== "undefined" && this.column.sortable === true);
     },
@@ -31,24 +36,19 @@ export default {
       return this.column.key === this.sortBy;
     },
     isSortedAsc() {
-      return this.isSorted && this.sortOrder === 'asc';
+      return this.sortOrder === 'asc';
     },
     isSortedDesc() {
-      return this.isSorted && this.sortOrder === 'desc';
-    },
-    isNumeric() {
-      return (typeof this.column.numeric !== "undefined" && this.column.numeric === true);
+      return this.sortOrder === 'desc';
     },
     getHeadCellClass() {
-      return [
-        'shapla-data-table__header-cell',
-        'shapla-data-table__header-cell-' + this.column.key,
-        {'column-primary': this.isPrimary},
-        {'is-numeric-cell': this.isNumeric},
-        {'is-sortable': this.column.sortable},
-        {'is-sorted-ascending': this.isSortedAsc},
-        {'is-sorted-descending': this.isSortedDesc}
-      ]
+      let classes = ['shapla-data-table__header-cell', `shapla-data-table__header-cell-${this.column.key}`];
+      if (this.isPrimary) classes.push('column-primary');
+      if (this.isNumeric) classes.push('is-numeric-cell');
+      if (this.isSortable) classes.push('is-sortable');
+      if (this.isSortedAsc) classes.push('is-sorted-ascending');
+      if (this.isSortedDesc) classes.push('is-sorted-descending');
+      return classes;
     }
   },
   methods: {
