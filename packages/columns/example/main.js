e9cb5d8c004631708eabd19ddcfb60c9282038bd
Sayful Islam <sayful.islam001@gmail.com>
2021-05-30 16:51:29 +0600

    Add columns component for vue 3

---
+++ packages/columns/example/main.js
@@ -0,0 +1,5 @@
+import {createApp} from 'vue'
+import ExampleColumns from './ExampleColumns.vue'
+
+const app = createApp(ExampleColumns);
+app.mount('#app');
---
+++ packages/columns/src/index.js
@@ -0,0 +1,16 @@
+/*!
+ * Shapla Columns v1.0.0
+ * (c) 2021 Sayful Islam
+ * Released under the MIT License.
+ */
+
+import Columns from './Columns.vue';
+import Column from './Column.vue';
+
+const Grid = {
+  row: Columns,
+  col: Column
+}
+
+export {Columns, Column}
+export default Grid;
