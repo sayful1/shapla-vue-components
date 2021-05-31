605f3d9a790205f925f3dd7f242283e5d552ec2b
Sayful Islam <sayful.islam001@gmail.com>
2021-05-31 10:23:52 +0600

    Publish ShaplaSpinner component for Vue 3

--- packages/spinner/src/index.js
+++ packages/spinner/src/index.js
@@ -1,9 +1,10 @@
 /*!
- * Shapla Cross v1.0.0
+ * Shapla Spinner v1.0.0
  * (c) 2021 Sayful Islam
  * Released under the MIT License.
  */
 
 import ShaplaSpinner from './ShaplaSpinner.vue';
 
+export {ShaplaSpinner}
 export default ShaplaSpinner;
