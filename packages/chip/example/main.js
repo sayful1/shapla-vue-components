832581a34aa595c91beaf6819b32cb9dc5c12666
Sayful Islam <sayful.islam001@gmail.com>
2021-06-01 23:01:26 +0600

    Publish @shapla/vue-chip component for Vue 3

---
+++ packages/chip/example/main.js
@@ -0,0 +1,5 @@
+import {createApp} from 'vue'
+import ExampleChip from './ExampleChip.vue'
+
+const app = createApp(ExampleChip);
+app.mount('#app');
---
+++ packages/chip/src/index.js
@@ -0,0 +1,9 @@
+/*!
+ * Shapla Chip v1.0.0
+ * (c) 2021 Sayful Islam
+ * Released under the MIT License.
+ */
+
+import ShaplaChip from './ShaplaChip.vue';
+
+export default ShaplaChip;
