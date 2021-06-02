11b6635f2eff1c1749cceb49024113f847d42c2a
Sayful Islam <sayful.islam001@gmail.com>
2021-06-02 09:58:49 +0600

    Publish @shapla/vue-icon component for Vue 3

---
+++ packages/icon/src/index.js
@@ -0,0 +1,10 @@
+/*!
+ * Shapla Icon v1.0.0
+ * (c) 2021 Sayful Islam
+ * Released under the MIT License.
+ */
+
+import ShaplaIcon from './ShaplaIcon.vue';
+
+export {ShaplaIcon}
+export default ShaplaIcon;
