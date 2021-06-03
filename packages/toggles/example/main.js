2e77fe772a13cc2e18b7ca7bf551e2e6edb748be
Sayful Islam <sayful.islam001@gmail.com>
2021-06-03 07:07:56 +0600

    Publish @shapla/vue-toggles component for Vue 3

---
+++ packages/toggles/example/main.js
@@ -0,0 +1,5 @@
+import {createApp} from 'vue'
+import TestToggles from './TestToggles.vue'
+
+const app = createApp(TestToggles);
+app.mount('#app');
---
+++ packages/toggles/src/index.js
@@ -0,0 +1,10 @@
+/*!
+ * Shapla Toggles v1.0.0
+ * (c) 2021 Sayful Islam
+ * Released under the MIT License.
+ */
+
+import ShaplaToggles from "./ShaplaToggles.vue";
+import ShaplaToggle from "./ShaplaToggle.vue";
+
+export {ShaplaToggles, ShaplaToggle}
