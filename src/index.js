3abac6c3c552dd52ed10c2acea206b313739137e
Sayful Islam <sayful.islam001@gmail.com>
2021-06-16 20:42:28 +0600

    Update component.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,31 @@
  * (c) 2021 Sayful Islam
  * Released under the MIT License.
  */
+// import './index.scss'
+
+export {default as ShaplaButton} from '@shapla/vue-button';
+export {default as ShaplaCheckbox} from '@shapla/vue-checkbox';
+export {default as ShaplaChip} from '@shapla/vue-chip';
+export {ShaplaColumns as ShaplaColumns, ShaplaColumn as ShaplaColumn} from '@shapla/vue-columns';
+export {Dialog as Dialog, ShaplaConfirmContainer as ShaplaConfirmContainer} from '@shapla/vue-confirm';
+export {default as ShaplaCross} from '@shapla/vue-cross';
+export {default as ShaplaDashboard} from '@shapla/vue-dashboard';
+export {ShaplaDropdown as ShaplaDropdown, ShaplaDropdownMenu as ShaplaDropdownMenu} from '@shapla/vue-dropdown';
+export {default as ShaplaIcon} from '@shapla/vue-icon';
+export {default as ShaplaImage} from '@shapla/vue-image';
+export {ShaplaModal as ShaplaModal, refreshBodyClass as refreshBodyClass} from '@shapla/vue-modal';
+export {Notify as Notify, ShaplaNotificationContainer as ShaplaNotificationContainer} from '@shapla/vue-notification';
+export {default as ShaplaProgress} from '@shapla/vue-progress';
+export {default as ShaplaSidenav} from '@shapla/vue-sidenav';
+export {default as ShaplaSpinner} from '@shapla/vue-spinner';
+export {default as ShaplaTable} from '@shapla/vue-table';
+export {ShaplaTabs as ShaplaTabs, ShaplaTab as ShaplaTab} from '@shapla/vue-tabs';
+export {ShaplaToggles as ShaplaToggles, ShaplaToggle as ShaplaToggle} from '@shapla/vue-toggles';
+export {default as ShaplaFileUploader} from '@shapla/vue-file-uploader';
+export {default as ShaplaInput} from '@shapla/vue-input';
+export {default as ShaplaRadio} from '@shapla/vue-radio';
+export {default as ShaplaRadioButton} from '@shapla/vue-radio-button';
+export {default as ShaplaSelect} from '@shapla/vue-select';
+export {default as ShaplaSlider} from '@shapla/vue-slider';
+export {default as ShaplaStarRating} from '@shapla/vue-star-rating';
+export {default as ShaplaSwitch} from '@shapla/vue-switch';
